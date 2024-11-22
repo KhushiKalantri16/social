cbf171a5af38c055a6e1dc153f6fee9c1cb6f66a
KhushiKalantri16 <117463389+KhushiKalantri16@users.noreply.github.com>
2024-11-22 16:24:43 +0530

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import ReactDOM from 'react-dom/client';
 import { useNavigate } from "react-router-dom";
 import "../Styles/Register.css";
 
